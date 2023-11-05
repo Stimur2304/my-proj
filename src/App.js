25a3bd5d5f1371cb135ce70dd192c1889295e9f1
Stimur2304 <samatov.timur10@gmail.com>
2023-11-05 17:39:38 +0600

    test

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
   const getWeather = async () =>{
     
     const response =  await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`)
+    
     const data = await response.json()
 
     try{
